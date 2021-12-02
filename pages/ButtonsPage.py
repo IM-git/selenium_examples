8244e01e34eab1333cb34a45af8d17a74f44224b
IM94 <hereimenkov@gmail.com>
2021-12-02 19:45:41 +0300

    Changed structure in ButtonsPage.py

--- pages/ButtonsPage.py
+++ pages/ButtonsPage.py
@@ -2,52 +2,26 @@ from .BasePage import BasePage
 from tools.MouseKeyboardActions import MouseKeyboardActions
 from selenium.webdriver.common.by import By
 
-LINK = 'https://demoqa.com/buttons'
-BUTTON_DOUBLE_CLICK_ME = '//button[@id="doubleClickBtn"]'
-GET_DOUBLE_CLICK_RESULT_TEXT = '//p[@id="doubleClickMessage"]'
-BUTTON_RIGHT_CLICK = '//button[@id="rightClickBtn"]'
-GET_RIGHT_CLICK_RESULT_TEXT = '//p[@id="rightClickMessage"]'
-BUTTON_CLICK = '//button[text()="Click Me"]'
-GET_CLICK_RESULT_TEXT = '//p[@id="dynamicClickMessage"]'
-
 
 class ButtonsPage(BasePage):
 
-    def open_buttons_page(self):
-        return self.open_page(LINK)
-
-    def wait_while_loaded_buttons_page(self):
-        self.wait_presence_of_element_located(
-            (By.XPATH, BUTTON_DOUBLE_CLICK_ME))
-
 # CHECK IS DISPLAYED
-    def check_is_displayed_double_click_button(self):
-        element = self.base_element.find_element_(
-            self.browser, By.XPATH, BUTTON_DOUBLE_CLICK_ME)
-        return self.base_element.check_is_displayed(element)
+    def check_is_displayed_double_click_button(self, browser, locator, element):
+        return self.base_element.check_is_displayed_2_0(browser, locator, element)
 
 # CLICK BUTTON
-    def click_double_click_button(self):
+    def click_double_click_button(self, locator, element):
         value = self.base_element.find_element_(
-            self.browser, By.XPATH, BUTTON_DOUBLE_CLICK_ME)
+            self.browser, locator, element)
         return MouseKeyboardActions(self.browser).double_click(value)
 
-    def click_one_right_button(self):
+    def click_one_right_button(self, locator, element):
         value = self.base_element.find_element_(
-            self.browser, By.XPATH, BUTTON_RIGHT_CLICK)
+            self.browser, locator, element)
         return MouseKeyboardActions(self.browser).right_click(value)
 
-    def click_button(self):
+    def click_button(self, locator, element):
         value = self.base_element.find_element_(
-            self.browser, By.XPATH, BUTTON_CLICK)
+            self.browser, locator, element)
         return MouseKeyboardActions(self.browser).one_click(value)
 
-# GET TEXT
-    def get_double_click_result_text(self):
-        return self.get_text(By.XPATH, GET_DOUBLE_CLICK_RESULT_TEXT)
-
-    def get_right_click_result_text(self):
-        return self.get_text(By.XPATH, GET_RIGHT_CLICK_RESULT_TEXT)
-
-    def get_click_result_text(self):
-        return self.get_text(By.XPATH, GET_CLICK_RESULT_TEXT)
